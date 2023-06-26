af3807d663f90ab63b400e04b3a1bbcd7e4916b0
Popo422 <105409876+Popo422@users.noreply.github.com>
2023-06-26 17:36:04 +0800

    Add files via upload

--- services/taskService/index.js
+++ services/taskService/index.js
@@ -1,1 +1,31 @@
+const queryAllTasks = require("./queryTask");
+const addNewTask = require("./addTask");
+const deleteSelectedTask = require("./deleteTask");
+const updateSelectedTasks = require("./updateTask");
+const queryAllTasksToClient = require("./queryAllTasks");
+
+module.exports.queryTasks = async (id) => {
+  const result = await queryAllTasks(id);
+  return result;
+};
+
+module.exports.addTask = async ( task) => {
+  const result = await addNewTask(task);
+  return result;
+};
+
+module.exports.deleteTask = async (id) => {
+  const result = await deleteSelectedTask(id);
+  return result;
+};
+module.exports.updateTask = async (id,task) => {
+  const result = await updateSelectedTasks(id,task);
+  return result;
+};
+
+module.exports.queryAllTasks = async (clientStartkey) => {
+  const result = await queryAllTasksToClient(clientStartkey);
+  return result;
+};
+
 
