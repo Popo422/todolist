708934f452c26e6cdf1877bde53ccf3862c70dd7
Popo422 <105409876+Popo422@users.noreply.github.com>
2023-06-26 17:42:11 +0800

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,1 +1,65 @@
 
+const express = require("express");
+const userRouter = express.Router();
+const bcrypt = require("bcryptjs");
+
+const {
+  queryAllTasks,
+  queryTasks,
+  addTask,
+  deleteTask,
+  updateTask,
+} = require("../services/usersService");
+
+userRouter.post("/addTask", async (req, res, next) => {
+  try {
+    const { task } = req.body;
+    const data = await addTask(task);
+    res.status(200).json(data);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+userRouter.post("/deleteTask", async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const data = await deleteTask(id);
+    res.status(200).json(data);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+userRouter.post("/updateTask", async (req, res, next) => {
+  try {
+    const { id,task } = req.body;
+    const data = await updateTask(id,task);
+    res.status(200).json(data);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+userRouter.post("/queryTask", async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const data = await queryTasks(id);
+    res.status(200).json(data);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+userRouter.post("/queryAllTask", async (req, res, next) => {
+  try {
+    const { clientStartkey } = req.body;
+    const items = await queryAllTasks(lastEvaluatedKey);
+    res.status(200).json(data);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = userRouter;
